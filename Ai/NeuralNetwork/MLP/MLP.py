e795b5c9a43291ccf0488affa7aae927e73f14a7
Valiance <lucas.ludovic@epitech.eu>
2025-04-02 10:06:27 +0200

    feat(AI): implement backward propagation for mlp

--- Ai/NeuralNetwork/MLP/MLP.py
+++ Ai/NeuralNetwork/MLP/MLP.py
@@ -36,3 +36,29 @@ class MLP(MyNeuralNetwork):
         self._z.append(output)
         self._a.append(output)
         return output
+
+    def propagateBackward(self, y_value, output):
+        weight_gradient = [None] * len(self._weight)
+        bias_gradient = [None] * len(self._bias)
+
+        delta = self.derivateLoss(output, y_value)
+
+        weight_gradient[-1] = np.dot(self._a[-2].T, delta)
+        bias_gradient[-1] = np.sum(delta, axis=0, keepdims=True)
+
+        for i in range(len(self._weight) - 2, - 1, - 1):
+            delta = np.dot(delta, self._weight[i + 1].T) * self.derivateRelu(self._z[i])
+            weight_gradient[i] = np.dot(self._a[i].T, delta)
+            bias_gradient[i] = np.sum(delta, axis=0, keepdims=True)
+        return weight_gradient, bias_gradient
+
+    def computeParams(self, weight_gradient, bias_gradient):
+        for i in range(len(self._weight)):
+            self._weight[i] -= self._alpha * weight_gradient
+            self._bias[i] -= self._alpha * bias_gradient
+
+    def computeLoss(y_pred, y_true):
+        return np.mean((y_pred, y_true) ** 2)
+
+    def derivateLoss(y_pred, y_true):
+        return 2 * (y_pred - y_true) / y_true.size
